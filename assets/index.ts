23a3609b3da28626b519b846e923a6080b1af7a7
Bismark Agyare <bismarkagyare54@gmail.com>
2025-02-05 14:48:36 +0000

    export categories images

---
+++ assets/index.ts
@@ -0,0 +1,15 @@
+import peachImage from "./category/peach.png";
+import vegetableImage from "./category/vegetable.png";
+import strawberryImage from "./category/strawberry.png";
+import appleImage from "./category/apple.png";
+import potatoImage from "./category/potato.png";
+import carrotImage from "./category/carrot.png";
+
+export {
+  peachImage,
+  vegetableImage,
+  strawberryImage,
+  appleImage,
+  potatoImage,
+  carrotImage,
+}; 

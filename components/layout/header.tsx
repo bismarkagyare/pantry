d826b32ddbd640286d8ba14449f109d709774879
Bismark Agyare <bismarkagyare54@gmail.com>
2025-02-05 14:48:52 +0000

    remove sticky class on header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,9 +2,10 @@ import { TopHeader } from "./top-header";
 import { MainHeader } from "./main-header";
 import { NavHeader } from "./nav-header";
 
+//sticky top-0 z-50
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white">
+    <header className="bg-white">
       <TopHeader />
       <MainHeader />
       <NavHeader />

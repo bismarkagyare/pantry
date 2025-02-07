723dc68aa5891c09fb14085ac497c296e231d4d3
Bismark Agyare <bismarkagyare54@gmail.com>
2025-02-07 13:35:50 +0000

    add loading skeletons

---
+++ components/ui/loading-skeleton.tsx
@@ -0,0 +1,18 @@
+export default function LoadingSkeleton() {
+  return (
+    <div className="container mx-auto py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {[...Array(8)].map((_, i) => (
+          <div key={i} className="bg-white rounded-lg shadow-md h-96 animate-pulse">
+            <div className="bg-gray-200 h-48 rounded-t-lg" />
+            <div className="p-4 space-y-3">
+              <div className="h-4 bg-gray-200 rounded w-3/4" />
+              <div className="h-3 bg-gray-200 rounded w-full" />
+              <div className="h-3 bg-gray-200 rounded w-1/2" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

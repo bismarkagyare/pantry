2872d0f905edc6821ae83006f54ef305203c66cc
Bismark Agyare <bismarkagyare54@gmail.com>
2025-02-15 18:15:34 +0000

    add category api router

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,4 +1,5 @@
 import { productRouter } from "./routers/product";
+import { categoryRouter } from "./routers/category";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   product: productRouter,
+  category: categoryRouter
 });
 
 // export type definition of API
